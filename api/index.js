c13a0668bc0445e853e289f7b343f817cee021da
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-08-26 20:40:12 +0200

    feat: implement the api

--- api/index.js
+++ api/index.js
@@ -14,43 +14,60 @@ app.use(express.json());
 
 app.post("/put", async function (req, res) {
   const id = uuid();
-  const queue = req.query.queue;
-  const timeoutMs = parseInt(req.query.timeout_ms);
-  const msg = req.body;
-  pool.query(
-    "INSERT INTO tasks (id, queue, msg, timeoutMs) VALUES (?, ?, ?, ?)",
-    id,
-    queue,
-    msg,
-    timeoutMs
+  const queue = ensureDefined(req.query.queue, "?queue");
+  const timeoutMs = parseInt(
+    ensureDefined(req.query.timeout_ms, "?timeout_ms")
   );
+  const msg = ensureDefined(req.body, "POST body");
+  await pool.query({
+    text: "INSERT INTO tasks (id, queue_path, msg, timeout_ms) VALUES ($1, $2, $3, $4)",
+    values: [id, queue, msg, timeoutMs],
+  });
   response.status(200).send(id);
 });
 
 app.get("/take", async function (req, res) {
-  const queue = req.query.queue;
-  const result = await pg.query(
-    "UPDATE tasks SET state='in_proggress', in_proggress_expire_at = NOW() + timeout WHERE state == wait AND queue = ? LIMIT 1 RETURNING *",
-    queue
-  );
-  response.status(200).send(result.msg);
+  const queue = ensureDefined(req.query.queue, "?queue");
+  const result = await pool.query({
+    text: "UPDATE tasks SET state_name = 'in_progress', in_progress_expire_at = extract(epoch from current_timestamp) * 1000 + extract(milliseconds from current_timestamp) + timeout_ms WHERE state = 'wait' AND queue = $1 LIMIT 1 RETURNING *",
+    values: [queue],
+  });
+  if (result.rows.length > 0) {
+    response.status(200).send(result.msg);
+  } else {
+    response.status(204).send();
+  }
 });
 
 app.get("/ack", async function (req, res) {
   const id = req.query.id;
-  pg.query("UPDATE tasks SET state=done, WHERE id = ?", id);
+  await pool.query({
+    text: "UPDATE tasks SET state='done' WHERE id = $1",
+    values: [id],
+  });
 });
 
 app.get("/nack", async function (req, res) {
   const id = req.query.id;
-  pg.query("UPDATE tasks SET state=failed, WHERE id = ?", id);
+  await pool.query({
+    text: "UPDATE tasks SET state='failed' WHERE id = $1",
+    values: [id],
+  });
 });
 
-callWithFrequency(1000 * ms, () => {
-  pg.query(
-    "UPDATE tasks SET state=waiting, retry_no = retry_no + 1 WHERE in_proggress_expire_at < NOW()"
-  );
-});
+async function returnTimeouts() {
+  try {
+    await pool.query(
+      "UPDATE tasks SET state='waiting', retry_no = retry_no + 1 WHERE in_progress_expire_at < (extract(epoch from current_timestamp) * 1000 + extract(milliseconds from current_timestamp))"
+    );
+    console.log("Return timeouts executed");
+  } catch (error) {
+    console.error("Return timeouts failed");
+    console.error(error);
+  }
+  setTimeout(returnTimeouts, 500);
+}
+setTimeout(returnTimeouts, 500);
 
 app.listen(80);
 
@@ -58,5 +75,11 @@ function requireEnv(name) {
   const value = process.env[name];
   if (typeof value === "undefined")
     throw new Error(`Env ${name} is not defined!`);
+  return value;
+}
+
+function ensureDefined(value, name) {
+  if (typeof value === "undefined")
+    throw new Error(`Value ${name} is not defined!`);
   return value;
 }
--- postgres/initdb/init.sql
+++ postgres/initdb/init.sql
@@ -5,12 +5,12 @@ CREATE TABLE tasks (
     queue_path VARCHAR(150) NOT NULL,
     state_name task_state DEFAULT 'wait',
     timeout_ms INT DEFAULT 3600000,
-    in_proggress_expire_at BIGINT DEFAULT 0,
+    in_progress_expire_at BIGINT DEFAULT 0,
     retry_no INT DEFAULT 0,
-    message NOT NULL LONGTEXT,
+    msg NOT NULL LONGTEXT,
 );
 
-CREATE INDEX tasks_in_proggress_expire_at_idx ON tasks (in_proggress_expire_at);
+CREATE INDEX tasks_in_progress_expire_at_idx ON tasks (in_progress_expire_at);
 CREATE INDEX tasks_id_idx ON tasks (id);
 CREATE INDEX tasks_state_idx ON tasks (queue_path, state_name);
 
