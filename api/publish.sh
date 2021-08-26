6f7022a9c111ae95b0898572796e07f23130fe46
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-08-26 20:21:50 +0200

    ci: api package publishing

---
+++ api/publish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+set -e
+
+BASE_TAG="jedrzejlewandowski/pg-task-broker-api"
+VERSION="1.0.0"
+
+docker build -t "${BASE_TAG}" .
+docker tag "${BASE_TAG}" "${BASE_TAG}:latest"
+docker tag "${BASE_TAG}" "${BASE_TAG}:${VERSION}"
+docker push "${BASE_TAG}:latest"
+docker push "${BASE_TAG}:${VERSION}"
