ec1dfe731eed4e5a2a4258458dd1f02557777c07
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-08-26 20:15:44 +0200

    feat: postgres setup

---
+++ postgres/initdb/init.sql
@@ -0,0 +1,17 @@
+CREATE TYPE task_state AS ENUM ('wait', 'in_progress', 'done', 'failed');
+
+CREATE TABLE tasks (
+    id VARCHAR(37) NOT NULL PRIMARY KEY,
+    queue_path VARCHAR(150) NOT NULL,
+    state_name task_state DEFAULT 'wait',
+    timeout_ms INT DEFAULT 3600000,
+    in_proggress_expire_at BIGINT DEFAULT 0,
+    retry_no INT DEFAULT 0,
+    message NOT NULL LONGTEXT,
+);
+
+CREATE INDEX tasks_in_proggress_expire_at_idx ON tasks (in_proggress_expire_at);
+CREATE INDEX tasks_id_idx ON tasks (id);
+CREATE INDEX tasks_state_idx ON tasks (queue_path, state_name);
+
+
--- postgres/publish.sh
+++ postgres/publish.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 set -e
 
-BASE_TAG="jedrzejlewandowski/pg-task-broker"
+BASE_TAG="jedrzejlewandowski/pg-task-broker-postgres"
 VERSION="1.0.0"
 
 docker build -t "${BASE_TAG}" .
